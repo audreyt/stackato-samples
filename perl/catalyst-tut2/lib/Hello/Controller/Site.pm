a8036ae11a20c95056a001dd8a8e5e95593d90a1
Gisle Aas <gisle@activestate.com>
2011-07-10 13:30:15 +0200

    Catalyst Tutorial Chapter2
    URL: http://dev.catalyst.perl.org/repos/Catalyst/trunk/examples/Tutorial/Hello_Chapter2.tar.gz
    Last Changed Author: hobbs
    Last Changed Rev: 13581
    Last Changed Date: 2010-09-03 05:39:59 +0200 (Fri, 03 Sep 2010)

---
+++ perl/catalyst-tut2/lib/Hello/Controller/Site.pm
@@ -0,0 +1,51 @@
+package Hello::Controller::Site;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Hello::Controller::Site - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Hello::Controller::Site in Site.');
+}
+
+
+sub test :Local {
+    my ( $self, $c ) = @_;
+
+    $c->stash(username => 'John',
+              template => 'site/test.tt');
+}
+
+
+=head1 AUTHOR
+
+root
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
